e6b418c45e66ab337b355b265811e5ab8ee7b00c
George <7jagjag@gmail.com>
2015-08-26 11:42:42 -0600

    colors

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,12 +1,36 @@
+# Reset
+OFF='\e[0m';       # Text Reset
 
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] $ "
-export CLICOLOR=1
-export LSCOLORS=ExFxBxDxCxegedabagacad
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
 
-# sync - conf
-export EDITOR=vim
+# Regular Colors
+Black='\e[0;30m';        # Black
+Red='\e[0;31m';          # Red
+Green='\e[0;32m';        # Green
+Yellow='\e[0;33m';       # Yellow
+Blue='\e[0;34m';         # Blue
+Purple='\e[0;35m';       # Purple
+Cyan='\e[0;36m';         # Cyan
+White='\e[0;37m';        # White
 
-# 
+export PS1="\[$Cyan\]\u\[$OFF\]@\[$Green\]\h:\[$Yellow\]\w \[$OFF\]¢ ";
+# export PS1="\[$Red\]\u\[$OFF\]@\[$Green\]\h:\[$Yellow\]\w \[$OFF\]\[$BRed\](warning!) \[$Red\]#\[$OFF\] ";
+
+export CLICOLOR=1;
+export LSCOLORS=ExFxBxDxCxegedabagacad;
+
+# Editor
+export EDITOR=vim;
+
+# Aliases
 alias ls='ls --color';
 alias l='clear;ls -l'
 alias la='clear;ls -Al';
@@ -15,7 +39,7 @@ alias search='la | grep';
 alias edit='$EDITOR ~/.bash_profile';
 alias back='cd ..; la';
 
-# git aliases
+# Git aliases
 alias gis='git status';
 alias gic='git checkout';
 alias gib='git branch';
